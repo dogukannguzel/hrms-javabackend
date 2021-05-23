efe8dccc3cba71b8c549e021722d3fde69a6160b
dogukannguzel <82955688+dogukannguzel@users.noreply.github.com>
2021-05-23 19:43:44 +0300

    Update MailService.java

--- hrms/src/main/java/kodlamaio/hrms/core/email/abstracts/MailService.java
+++ hrms/src/main/java/kodlamaio/hrms/core/email/abstracts/MailService.java
@@ -1,13 +1,12 @@
 package kodlamaio.hrms.core.email.abstracts;
 
-import kodlamaio.hrms.core.utilities.results.Result;
+
 import kodlamaio.hrms.entities.abstracts.User;
-import kodlamaio.hrms.entities.concretes.Candidate;
+
 
 public interface MailService {
 
-    void sendMail(User user
-    );
+    void sendMail(User user);
 
 
 
